b9e69d153c53669b00af89cde332c13c4bbc8e92
josuegmo97 <ejmgabriel97@gmail.com>
2024-05-16 11:26:05 -0400

    feat: delete files unnecesary

--- src/user/addInstructorUser.js
+++ src/user/addInstructorUser.js
@@ -4,32 +4,43 @@ const { jwtMiddleware } = require('../../middleware/jwt');
 
 const addInstructorUser = async (event) => {
 
-  const jwtResult = await jwtMiddleware(event, true);
-  if (jwtResult) { return jwtResult; }
+  try {
+    const jwtResult = await jwtMiddleware(event, true);
+    if (jwtResult) { return jwtResult; }
+    
+    await connectToDatabase();
+
+    const { fullName, email, phone, documentType, document, birthday  } = JSON.parse(event.body);
+
+    const query = {
+      fullName,
+      email,
+      phone,
+      document_type: documentType,
+      document,
+      birthday,
+      rol: 'instructor',
+      course: null,
+      created_by: event.user._id
+    };
+
+    const newInstructor = new User(query);
+    await newInstructor.save();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(newInstructor)
+    };
+  } catch (error) {
+    
+    console.log(error);
+
+    return {
+      statusCode: 400,
+      body: 'Error al intentar crear el instructor'
+    };
+  }
   
-  await connectToDatabase();
-
-  const { fullName, email, phone, documentType, document, birthday  } = JSON.parse(event.body);
-
-  const query = {
-    fullName,
-    email,
-    phone,
-    document_type: documentType,
-    document,
-    birthday,
-    rol: 'instructor',
-    course: null,
-    created_by: event.user._id
-  };
-
-  const newInstructor = new User(query);
-  await newInstructor.save();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(newInstructor)
-  };
 
 
 }
